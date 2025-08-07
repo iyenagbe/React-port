60857dc007dabc469cef60bbf0e87a1c0e79130f
iyenagbe <iyenagberaymond@gmail.com>
2025-08-07 14:53:56 +0100

    make changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@Lib': path.resolve(__dirname, 'src/Lib'),
